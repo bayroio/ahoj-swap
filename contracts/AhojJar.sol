6d65baf91ff0649343e352e5c2221df4565ad3a2
Alan <Alan.Escudero.Caporal@gmail.com>
2021-01-20 21:21:17 -0600

    Case B completed

--- contracts/AhojJar.sol
+++ contracts/AhojJar.sol
@@ -9,12 +9,6 @@ contract AhojJar is IAhojJar {
     address public token1;
     address public token2;
 
-    uint private reserves1;
-    uint private reserves2;
-
-    uint private value1;
-    uint private value2;
-
     uint private FEE_PORCENT = 30; // 10000 Are equals to 100%
 
     event Swap(address indexed token, uint amount, address indexed to);
@@ -23,8 +17,6 @@ contract AhojJar is IAhojJar {
     constructor(address _token1, address _token2) public {
         token1 = _token1;
         token2 = _token2;
-        value1 = 100000; // 1 TokenA = 10 Dollar (four ceros for decimals)
-        value2 = 20000; // 1 TokenB = 2 Dollars (four ceros for decimals)
     }
 
     function getReserves() public view override returns (uint _reserves1, uint _reserves2) {
@@ -37,28 +29,22 @@ contract AhojJar is IAhojJar {
         (uint _reserves1, uint _reserves2) = getReserves();
         IERC20 _token1 = IERC20(token1);
         IERC20 _token2 = IERC20(token2);
-        /*
         if(_amountX > 0) {
             require(_token1.allowance(msg.sender, address(this)) == _amountX, 
             'ERR: Allowance was not Made or not is Exactly the ammount required');
-            uint swapAmmount = Math.getSwapAmmount(_amountX, _value1, _value2);
-            require(_amountX < _reserves1 && swapAmmount < _reserves2, 'ERR: Insuficient Liquidity');
+            uint _swapAmmount = Math.getSwapAmmount(_reserves2, _reserves1, FEE_PORCENT, _amountX);
+            require(_swapAmmount < _reserves2, 'ERR: Insuficient Liquidity');
             transferAllowance(_token1, _amountX);
-            transferSwap(_token2, swapAmmount);
+            transferSwap(_token2, _swapAmmount);
         }
-        */
         if(_amountY > 0) {
-            //emit Alert("MSG: B",_amountY);
             require(_token2.allowance(msg.sender, address(this)) == _amountY, 
             'ERR: Allowance was not Made or not is Exactly the ammount required');
             uint _swapAmmount = Math.getSwapAmmount(_reserves1, _reserves2, FEE_PORCENT, _amountY);
             require(_swapAmmount < _reserves1, 'ERR: Insuficient Liquidity');
             transferAllowance(_token2, _amountY);
             transferSwap(_token1, _swapAmmount);
-            emit Alert("MSG: A", _swapAmmount);
         }
-        reserves1 = _token1.balanceOf(address(this));
-        reserves2 = _token2.balanceOf(address(this));
     }
     
     function transferAllowance(IERC20 _token, uint _amount) private {
@@ -70,9 +56,10 @@ contract AhojJar is IAhojJar {
         emit Swap(address(_token), _amount, msg.sender);
     }
 
-    function updatePrices(uint _value1, uint _value2) private {
+    function getChangeValue() public view returns(uint _result) {
         (uint _reserves1, uint _reserves2) = getReserves();
-        //Aqui toca trabajar
+        uint result = _reserves2/_reserves1;
+        _result = result;
     }
     
 }
--- contracts/libraries/Math.sol
+++ contracts/libraries/Math.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.6.0;
 
 library Math {
-
+/*
     function getEfectivePrice(uint _assetIn, uint _assetOut, uint _valueInput, uint _fee) internal pure returns (uint _valueOutput) {
         uint product = _assetIn * _assetOut;
         _valueOutput = (_assetOut - product/(_assetIn + _valueInput));
         //_valueOutput = (_assetOut - product/(_assetIn + _valueInput * (100 - _fee)));
     }
-
+*/
     function getSwapAmmount(uint _assetOut, uint _assetIn,uint _fee, uint _input) internal pure returns (uint _result) {
         uint _product = _assetOut * _assetIn;
         uint _part1 = (_input*(10000 - _fee))/(10**4);
